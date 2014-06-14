06e8f248efa0d30b18784ae41410bf0325234308
Joshua Howland <development@jkhowland.com>
2014-06-13 22:11:01 -0600

    Adds block syntax

--- BlockHead/BHViewController.m
+++ BlockHead/BHViewController.m
@@ -10,6 +10,9 @@
 
 @interface BHViewController ()
 
+@property (nonatomic, copy) NSArray *(^stringArray)(NSString *);
+@property (nonatomic, copy) void (^someBlock)(int);
+
 @end
 
 @implementation BHViewController
@@ -17,7 +20,69 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+
+    NSString *stringLiteral = @"string";
+
+    
+    
+    // Block as Local Variable no Return Type
+    
+    void (^block)() = ^{
+        NSLog(@"%@",stringLiteral);
+    };
+
+    block();
+    
+    
+    // Block as Local Variable with Return Type
+    
+    int (^multiply)(int, int) = ^(int one, int two) {
+        int multiple = one * two;
+        NSLog(@"%d", multiple);
+        return multiple;
+    };
+    
+    multiply(5, 4);
+
+
+    // Property set to a block literal
+    
+    self.stringArray = ^(NSString *string){
+        return @[string];
+    };
+    
+    NSArray *strings = self.stringArray(stringLiteral);
+    if ([strings.firstObject isEqualToString:stringLiteral]) {
+        NSLog(@"Success");
+    }
+    
+    self.someBlock = ^(int value){
+        NSLog(@"%d blocked", value);
+    };
+    
+    self.someBlock(25);
+ 
+    
+    // Using blocks as arguments in a method call
+    
+    [self someMethodThatTakesABlockWithParameter:^(int parameter) {
+        NSLog(@"%d integer method called", parameter);
+    }];
+    
+    [self someMethodThatTakesABlock:^{
+        NSLog(@"Method without parameter called");
+    }];
+    
+}
+
+// Methods with blocks as the parameter
+
+- (void)someMethodThatTakesABlockWithParameter:(void (^)(int parameter))block {
+    block(100);
+}
+
+- (void)someMethodThatTakesABlock:(void (^)())block {
+    block();
 }
 
 - (void)didReceiveMemoryWarning
